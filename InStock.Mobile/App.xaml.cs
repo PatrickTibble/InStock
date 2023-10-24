68b07c42fd19faa6d19a2808082e05f3a8f9d4e6
PatrickTibble <ptibble88@gmail.com>
2023-10-24 13:08:08 -0500

    initial commit

---
+++ InStock.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InStock.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ InStock.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InStock.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
