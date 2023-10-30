4c2a56c0bbb11f72bfaacd9ac053c8bc04d7133f
Patrick Tibble <p.tibble@live.com>
2023-10-30 11:49:33 -0500

    add MockService resolver and mock services

--- InStock.Frontend.API/APIServiceRegistrar.cs
+++ InStock.Frontend.API/APIServiceRegistrar.cs
@@ -5,6 +5,10 @@ namespace InStock.Frontend.API
 	public class APIServiceRegistrar : IServiceRegistrar
 	{
         public T GetService<T>(HttpClient client)
+#if USE_MOCKS
+            => MockService.For<T>();
+#else
             => RestService.For<T>(client);
+#endif
     }
 }
---
+++ InStock.Frontend.API/Mocks/BaseMockService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InStock.Frontend.API.Mocks
+{
+    internal abstract class BaseMockService
+    {
+        private readonly Random _random = new Random(DateTime.UtcNow.Millisecond);
+
+        /// <summary>
+        /// Simulates a network delay between min-max
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        protected Task Delay(CancellationToken token, int min = 100, int max = 250)
+        {
+            var millisecondDelay = _random.Next(min, max);
+            var timespanDelay = TimeSpan.FromMilliseconds(millisecondDelay);
+            return Task.Delay(timespanDelay, token);
+        }
+    }
+}
