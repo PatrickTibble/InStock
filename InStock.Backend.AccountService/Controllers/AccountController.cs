9f2479a34c3787c7c61b514b76c52572a6cdf463
Patrick Tibble <p.tibble@live.com>
2023-11-02 11:44:36 -0500

    start account api dev -> SessionState

---
+++ InStock.Backend.AccountService/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace InStock.Backend.AccountService.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/v1/Account/SessionState")]
+        [SwaggerOperation(Description = "Returns user session state")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(Common.Models.Account.SessionStatus.Response), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetSessionStateAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+    }
+}
