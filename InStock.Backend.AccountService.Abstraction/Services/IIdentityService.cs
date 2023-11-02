ec7cc81dfafd7bf9c2d9501f149f4ef5d0a4018b
Patrick Tibble <p.tibble@live.com>
2023-11-02 15:45:53 -0500

    add identity service, (should go to its own service)

---
+++ InStock.Backend.AccountService.Abstraction/Services/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿namespace InStock.Backend.AccountService.Abstraction.Services
+{
+    public enum UserClaim
+    {
+        Session_Read
+    }
+
+    public interface IIdentityService
+    {
+        Task<IEnumerable<UserClaim>> GetUserClaimsAsync(string accessToken);
+        Task<string> VerifyUserCredentialsAsync(string username, string password, IList<string> claims);
+    }
+}
