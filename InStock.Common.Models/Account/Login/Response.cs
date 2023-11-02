b1fb50f47b16bb690c52a5847b07145272158cdc
Patrick Tibble <p.tibble@live.com>
2023-11-02 15:45:32 -0500

    add access token to login response

--- InStock.Common.Models/Account/Login/Response.cs
+++ InStock.Common.Models/Account/Login/Response.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using InStock.Common.Models.Base;
+﻿using InStock.Common.Models.Base;
 
 namespace InStock.Common.Models.Account.Login
 {
 	public class Response : IResponse
 	{
-		public bool IsSuccessfulStatusCode { get; } = true;
-	}
+        public static object? Default { get; set; }
+        public bool IsSuccessfulStatusCode { get; set; }
+        public string AccessToken { get; set; }
+    }
 }
-
