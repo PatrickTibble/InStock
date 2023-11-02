9c18d57eb189db9916bb88b45cc86725c8f6b9ce
Patrick Tibble <p.tibble@live.com>
2023-11-02 14:49:48 -0500

    add core layer to implement abstractions

---
+++ InStock.Backend.AccountService.Core/Services/Account/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using InStock.Backend.AccountService.Abstraction.Services;
+
+namespace InStock.Backend.AccountService.Core.Services.Account
+{
+    public class AccountService : IAccountService
+    {
+        public Task<Common.Models.Account.CreateAccount.Response> CreateAccountAsync(Common.Models.Account.CreateAccount.Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Common.Models.Account.SessionStatus.Response> GetSessionStateAsync(string accessToken)
+        {
+            // retrieve user information using access token
+
+            // retrieve session information using user info
+
+            // return session info as response
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InStock.Backend.AccountService.Core/Services/Account/MockAccountService.cs
@@ -0,0 +1,17 @@
+﻿using InStock.Backend.AccountService.Abstraction.Services;
+
+namespace InStock.Backend.AccountService.Core.Services.Account
+{
+    public class MockAccountService : IAccountService
+    {
+        public Task<Common.Models.Account.CreateAccount.Response> CreateAccountAsync(Common.Models.Account.CreateAccount.Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Common.Models.Account.SessionStatus.Response> GetSessionStateAsync(string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
