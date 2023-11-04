62af709b2c193fa26484f9f5769f8409d7021b31
Patrick Tibble <ptibble88@gmail.com>
2023-11-04 11:57:31 -0500

    update service - DTO namepsaces

--- InStock.Backend.IdentityService.Core/Services/Identity/IdentityService.cs
+++ InStock.Backend.IdentityService.Core/Services/Identity/IdentityService.cs
@@ -1,42 +1,78 @@
 ﻿using InStock.Backend.IdentityService.Abstraction.Entities;
 using InStock.Backend.IdentityService.Abstraction.Repositories;
 using InStock.Backend.IdentityService.Abstraction.Services;
+using InStock.Backend.IdentityService.Abstraction.TransferObjects.Authenticate;
+using InStock.Backend.IdentityService.Abstraction.TransferObjects.Register;
+using InStock.Backend.IdentityService.Abstraction.TransferObjects.SendVerificationLink;
+using InStock.Backend.IdentityService.Abstraction.TransferObjects.VerifyEmail;
 
 namespace InStock.Backend.IdentityService.Core.Services.Identity
 {
     public class IdentityService : IIdentityService
     {
+        private readonly ICommunicationService _communicationService;
         private readonly IIdentityRepository _identityRepository;
 
-        public IdentityService(IIdentityRepository identityRepository)
+        public IdentityService(
+            IIdentityRepository identityRepository,
+            ICommunicationService communicationService)
         {
+            _communicationService = communicationService;
             _identityRepository = identityRepository;
         }
 
-        public async Task<IEnumerable<UserClaim>> GetUserClaimsAsync(string accessToken)
+        public async Task<IEnumerable<UserClaim>> GetUserClaimsAsync(string accessToken, CancellationToken? token = null)
         {
-            var claims = await _identityRepository.GetUserClaimsAsync(accessToken);
+            var claims = await _identityRepository.GetUserClaimsAsync(accessToken, token);
             return claims;
         }
 
-        public async Task<Abstraction.TransferObjects.Register.Response> RegisterUserAsync(Abstraction.TransferObjects.Register.Request request)
+        public async Task<RegistrationResponse> RegisterUserAsync(RegistrationRequest request, CancellationToken? token = null)
         {
-            var user = await _identityRepository.RegisterUserAsync(request);
+            var result = await _identityRepository.RegisterUserAsync(request.Username, request.Password, token);
+
+            var response = new RegistrationResponse
+            {
+                IsRegistered = result
+            };
+
+            return response;
         }
 
-        public Task<Abstraction.TransferObjects.SendVerificationLink.Response> SendVerificationLinkAsync(Abstraction.TransferObjects.SendVerificationLink.Request request)
+        public async Task<VerificationLinkResponse> SendVerificationLinkAsync(VerificationLinkRequest request, CancellationToken? token = null)
         {
-            throw new NotImplementedException();
+            var result = await _communicationService.SendVerificationLinkAsync(request, token);
+
+            var response = new VerificationLinkResponse
+            {
+                IsSent = result
+            };
+
+            return response;
         }
 
-        public Task<Abstraction.TransferObjects.VerifyEmail.Response> VerifyEmailAsync(Abstraction.TransferObjects.VerifyEmail.Request request)
+        public async Task<VerifyEmailResponse> VerifyEmailAsync(VerifyEmailRequest request, CancellationToken? token = null)
         {
-            throw new NotImplementedException();
+            var result = await _communicationService.VerifyEmailAsync(request, token);
+
+            var response = new VerifyEmailResponse
+            {
+                IsVerified = result
+            };
+
+            return response;
         }
 
-        public Task<Abstraction.TransferObjects.Authenticate.Response> VerifyUserCredentialsAsync(Abstraction.TransferObjects.Authenticate.Request request, List<string> claims)
+        public async Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request, List<string> claims, CancellationToken? token = null)
         {
-            throw new NotImplementedException();
+            var result = await _identityRepository.VerifyUserCredentialsAsync(request.Username, request.Password, claims, token);
+
+            var response = new AuthenticationResponse
+            {
+                AccessToken = result
+            };
+
+            return response;
         }
     }
 }
