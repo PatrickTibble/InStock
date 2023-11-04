57916d12c126a9bc0c47f604dc5c2a6d1dae60e6
Patrick Tibble <ptibble88@gmail.com>
2023-11-04 11:57:02 -0500

    add communication service

---
+++ InStock.Backend.IdentityService.Abstraction/Services/ICommunicationService.cs
@@ -0,0 +1,11 @@
+﻿using InStock.Backend.IdentityService.Abstraction.TransferObjects.SendVerificationLink;
+using InStock.Backend.IdentityService.Abstraction.TransferObjects.VerifyEmail;
+
+namespace InStock.Backend.IdentityService.Abstraction.Services
+{
+    public interface ICommunicationService
+    {
+        Task<bool> SendVerificationLinkAsync(VerificationLinkRequest request, CancellationToken? token);
+        Task<bool> VerifyEmailAsync(VerifyEmailRequest request, CancellationToken? token);
+    }
+}
