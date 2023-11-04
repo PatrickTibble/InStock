ee210fed6da2bea68a4a237b2ec9c3713325cefb
Patrick Tibble <ptibble88@gmail.com>
2023-11-04 13:29:22 -0500

    add UserRole to HashedUser

--- InStock.Backend.IdentityService.Abstraction/Entities/HashedUser.cs
+++ InStock.Backend.IdentityService.Abstraction/Entities/HashedUser.cs
@@ -3,6 +3,7 @@
     public class HashedUser
     {
         public string Username { get; set; }
+        public UserRole Role { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
     }
