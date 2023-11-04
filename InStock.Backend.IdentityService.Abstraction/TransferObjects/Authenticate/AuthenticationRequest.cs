ed414ffbe5d2d1675139e9a18d2fe8f5f7301141
Patrick Tibble <ptibble88@gmail.com>
2023-11-04 12:29:54 -0500

    start adding some validation requirements

--- InStock.Backend.IdentityService.Abstraction/TransferObjects/Authenticate/AuthenticationRequest.cs
+++ InStock.Backend.IdentityService.Abstraction/TransferObjects/Authenticate/AuthenticationRequest.cs
@@ -1,10 +1,16 @@
 ﻿using InStock.Backend.IdentityService.Abstraction.TransferObjects.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace InStock.Backend.IdentityService.Abstraction.TransferObjects.Authenticate
 {
     public class AuthenticationRequest : BaseRequest
     {
+        [Required]
+        [MinLength(8, ErrorMessage = "Username is too short")]
         public string? Username { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password is too short")]
         public string? Password { get; set; }
     }
 }
