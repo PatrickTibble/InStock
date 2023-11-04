0f938da6278634d7bb77d7108d53e5eb35df026e
Patrick Tibble <ptibble88@gmail.com>
2023-11-04 14:10:38 -0500

    add UnitTests project for IdentityService

---
+++ InStock.Backend.Tests.IdentityService.UnitTests/Controllers/IdentityControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using InStock.Backend.IdentityService.Abstraction.Services;
+using InStock.Backend.IdentityService.Controllers;
+using Moq;
+
+namespace InStock.Backend.Tests.IdentityService.UnitTests.Controllers
+{
+    internal class IdentityControllerTests
+    {
+        private Mock<IIdentityService> _identityService;
+        private IdentityController _controller;
+
+        public void Setup()
+        {
+            _identityService = new Mock<IIdentityService>();
+            _controller = new IdentityController(
+                _identityService.Object);
+        }
+    }
+}
