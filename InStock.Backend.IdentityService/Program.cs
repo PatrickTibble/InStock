de422267e946d95760e90470f3c31ecac9f3476c
Patrick Tibble <ptibble88@gmail.com>
2023-11-07 08:35:03 -0600

    register services

--- InStock.Backend.IdentityService/Program.cs
+++ InStock.Backend.IdentityService/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
+    .AddSingleton<IHashService, HmacSha512HashService>()
+    .AddSingleton<ITokenService, JwtSecurityTokenService>()
     .AddSingleton<IIdentityRepository, IdentityRepository>()
     .AddSingleton<IIdentityService, IdentityService>()
     
