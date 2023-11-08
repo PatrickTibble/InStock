d506360fe6b6e5790c1d798ea72f8c565d4f6235
Patrick Tibble <ptibble88@gmail.com>
2023-11-07 21:27:09 -0600

    update constants

--- InStock.Common.IdentityService.Abstraction/Constants.cs
+++ InStock.Common.IdentityService.Abstraction/Constants.cs
@@ -8,6 +8,15 @@
         public const string Authenticate = "api/v1/Identity/Authenticate";
         public const string UserClaims = "api/v1/Identity/UserClaims";
 
+        public const string UserProfile = "api/v1/User/UserProfile";
+        public const string UserAddress = "api/v1/User/Address";
+        public const string UserAddresses = "api/v1/User/UserAddresses";
+
+        public const string SessionState = "api/v1/Session/SessionState";
+        public const string ContinueSession = "api/v1/Session/Continue";
+        public const string EndSession = "api/v1/Session/End";
+        public const string CreateSession = "api/v1/Session/Create";
+
         public const string BaseUrl = "https://localhost:44315";
     }
 }
