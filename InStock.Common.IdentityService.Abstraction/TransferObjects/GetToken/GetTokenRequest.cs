ec4352e0762e1103af3bcc62d8b72a1194ae8741
Patrick Tibble <ptibble88@gmail.com>
2023-11-09 18:19:54 -0600

    add nullable and defaults

--- InStock.Common.IdentityService.Abstraction/TransferObjects/GetToken/GetTokenRequest.cs
+++ InStock.Common.IdentityService.Abstraction/TransferObjects/GetToken/GetTokenRequest.cs
@@ -3,7 +3,7 @@
     public class GetTokenRequest
     {
         public DateTime Expiry { get; set; }
-        public IDictionary<string, string> Claims { get; set; }
-        public string Username { get; set; }
+        public IDictionary<string, string>? Claims { get; set; }
+        public string Username { get; set; } = string.Empty;
     }
 }
