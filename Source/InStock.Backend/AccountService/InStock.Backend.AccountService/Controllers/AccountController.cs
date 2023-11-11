0e23bac8492a0c45ae2526cb5cf6b635c0e0baa8
Patrick Tibble <ptibble88@gmail.com>
2023-11-11 08:28:45 -0600

    include Result in response model

--- Source/InStock.Backend/AccountService/InStock.Backend.AccountService/Controllers/AccountController.cs
+++ Source/InStock.Backend/AccountService/InStock.Backend.AccountService/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using InStock.Common.AccountService.Abstraction.TransferObjects.Login;
 using InStock.Backend.Common.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
+using InStock.Common.Models.Base;
 
 namespace InStock.Backend.AccountService.Controllers
 {
@@ -24,7 +25,7 @@ namespace InStock.Backend.AccountService.Controllers
         [SwaggerOperation(Description = "Attempt account log in with credentials")]
         [Produces(MediaTypeNames.Application.Json)]
         [Consumes(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Result<LoginResponse>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> LoginAsync(
             [FromBody] LoginRequest request)
@@ -34,7 +35,10 @@ namespace InStock.Backend.AccountService.Controllers
                 return BadRequest(ModelState);
             }
 
-            var result = await _accountService.LoginAsync(request);
+            var result = await _accountService
+                .LoginAsync(request)
+                .ConfigureAwait(false);
+
             return result.ToActionResult();
         }
 
@@ -43,7 +47,7 @@ namespace InStock.Backend.AccountService.Controllers
         [SwaggerOperation(Description = "Create a User Account")]
         [Produces(MediaTypeNames.Application.Json)]
         [Consumes(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(typeof(CreateAccountResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Result<LoginResponse>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> CreateUserAccountAsync(
@@ -54,7 +58,10 @@ namespace InStock.Backend.AccountService.Controllers
                 return BadRequest(ModelState);
             }
 
-            var result = await _accountService.CreateAccountAsync(request);
+            var result = await _accountService
+                .CreateAccountAsync(request)
+                .ConfigureAwait(false);
+
             return result.ToActionResult();
         }
     }
