d7bd97b71488a2a3dea30a58956592b064f83fd3
Patrick Tibble <ptibble88@gmail.com>
2023-11-11 21:45:21 -0600

    add persistent storage impl

--- Source/InStock.Frontend/InStock.Frontend.Mobile/Services/Settings/SettingsService.cs
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Services/Settings/SettingsService.cs
@@ -4,19 +4,20 @@ namespace InStock.Frontend.Mobile.Services.Settings
 {
     public class SettingsService : ISettingsService
     {
+
         public Task<T> GetValueOrDefaultAsync<T>(string key, T? defaultValue = default)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.FromResult(Preferences.Default.Get(key, defaultValue));
 
         public Task<bool> TryRemoveValueAsync(string key)
         {
-            throw new NotImplementedException();
+            Preferences.Default.Remove(key);
+            return Task.FromResult(true);
         }
 
         public Task<bool> TrySetValueAsync<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            Preferences.Default.Set(key, value);
+            return Task.FromResult(true);
         }
     }
 }
