7ebc6be5636aa8fd709e7f95e46eeafe0c155fc3
Patrick Tibble <ptibble88@gmail.com>
2023-11-12 21:31:55 -0600

    refactor to share properties

--- Source/InStock.Common/AccountService/InStock.Common.AccountService.Abstraction/TransferObjects/Login/LoginRequest.cs
+++ Source/InStock.Common/AccountService/InStock.Common.AccountService.Abstraction/TransferObjects/Login/LoginRequest.cs
@@ -5,10 +5,22 @@ namespace InStock.Common.AccountService.Abstraction.TransferObjects.Login
     public class LoginRequest
 	{
         [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters")]
         public string? Username { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters")]
         public string? Password { get; set; }
-        public string ClientId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Client ID is required")]
+        public Guid ClientId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Client name is required")]
+        [MinLength(3, ErrorMessage = "Client name must be at least 3 characters")]
+        public string? ClientName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Client description is required")]
+        [MinLength(10, ErrorMessage = "Client name must be at least 10 characters")]
+        public string? ClientDescription { get; set; }
     }
 }
