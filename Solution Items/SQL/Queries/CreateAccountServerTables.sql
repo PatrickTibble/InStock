bd1269937b87cb5c62c1d87001a2ddc44db2029a
Patrick Tibble <ptibble88@gmail.com>
2023-11-13 18:35:26 -0600

    update account server tables

--- Solution Items/SQL/Queries/CreateAccountServerTables.sql
+++ Solution Items/SQL/Queries/CreateAccountServerTables.sql
@@ -1,15 +1,50 @@
-﻿use [dbo.AccountService];
+﻿use [InStock.AccountServer];
 
 CREATE TABLE Users (
-    Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
-    Username NVARCHAR(50) NOT NULL UNIQUE,
-    PasswordHash VARBINARY(1024) NOT NULL,
-    PasswordSalt VARBINARY(1024) NOT NULL
+    Id              INT             NOT NULL IDENTITY(1,1),
+    Username        NVARCHAR(50)    NOT NULL UNIQUE,
+    PasswordHash    VARBINARY(1024) NOT NULL,
+    PasswordSalt    VARBINARY(1024) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
 CREATE TABLE UserProfiles (
-    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    UserId INT FOREIGN KEY REFERENCES Users(Id),
-    FirstName NVARCHAR(50) NOT NULL,
-    LastName NVARCHAR(50) NOT NULL
+    Id          INT             IDENTITY(1,1) NOT NULL,
+    UserId      INT             NOT NULL,
+    FirstName   NVARCHAR(50)    NOT NULL,
+    LastName    NVARCHAR(50)    NOT NULL,
+    CONSTRAINT [PK_UserProfiles] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserProfiles_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+
+CREATE TABLE [dbo].[Metadata] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Metadata] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Clients] (
+    [Id]         INT              IDENTITY (1, 1) NOT NULL,
+    [MetadataId] INT              NOT NULL,
+    [Uid]        UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Clients_Metadata] FOREIGN KEY ([MetadataId]) REFERENCES [dbo].[Metadata] ([Id])
+);
+
+CREATE TABLE [dbo].[UserClients] (
+    [Id]       INT              IDENTITY (1, 1) NOT NULL,
+    [UserId]   INT              NOT NULL,
+    [ClientId] INT              NOT NULL,
+    CONSTRAINT [PK_UserClients] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserClients_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
+    CONSTRAINT [FK_UserClients_Clients] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Clients] ([Id])
+);
+
+CREATE TABLE [dbo].[UserClientTokens] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [UserClientId]  INT            NOT NULL,
+    [IdentityToken] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_UserClientTokens] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserClientTokens_UserClients] FOREIGN KEY ([UserClientId]) REFERENCES [dbo].[UserClients] ([Id])
 );
