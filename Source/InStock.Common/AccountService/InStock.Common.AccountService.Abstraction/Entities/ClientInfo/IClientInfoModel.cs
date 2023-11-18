340546b04819aa810118e6599459a2b3a767d36d
Patrick Tibble <ptibble88@gmail.com>
2023-11-18 09:51:16 -0600

    add interface contract for client info

---
+++ Source/InStock.Common/AccountService/InStock.Common.AccountService.Abstraction/Entities/ClientInfo/IClientInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InStock.Common.AccountService.Abstraction.Entities.ClientInfo
+{
+    public interface IClientInfoModel
+    {
+        Guid ClientId { get; set; }
+        string? ClientName { get; set; }
+        string? ClientDescription { get; set; }
+    }
+}
