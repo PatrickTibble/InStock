7e6b1f0cade8cd9cf9b7e983381917e3a7b66e20
Patrick Tibble <ptibble88@gmail.com>
2023-11-18 09:15:56 -0600

    Platform info service. TODO: combine with client info service?

---
+++ Source/InStock.Frontend/InStock.Frontend.Abstraction/Services/Platform/IPlatformInfoService.cs
@@ -0,0 +1,8 @@
+﻿namespace InStock.Frontend.Abstraction.Services.Platform
+{
+    public interface IPlatformInfoService
+    {
+        string ClientName { get; }
+        string ClientDescription { get; }
+    }
+}
---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Services/Platform/PlatformInfoService.cs
@@ -0,0 +1,11 @@
+﻿using InStock.Frontend.Abstraction.Services.Platform;
+
+namespace InStock.Frontend.Mobile.Services.Platforms
+{
+    public class PlatformInfoService : IPlatformInfoService
+    {
+        public string ClientName { get; } = $"[{DeviceInfo.Platform}] {DeviceInfo.Manufacturer} {DeviceInfo.Model}";
+
+        public string ClientDescription { get; } = $"{DeviceInfo.DeviceType} {DeviceInfo.VersionString} {DeviceInfo.Idiom}";
+    }
+}
