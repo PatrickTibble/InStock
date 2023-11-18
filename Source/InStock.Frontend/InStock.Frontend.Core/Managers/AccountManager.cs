6f7207360005ef14193c2ff1b170d08cbe5f3ed6
Patrick Tibble <ptibble88@gmail.com>
2023-11-18 09:51:30 -0600

    trim down duplicated code and logic

--- Source/InStock.Frontend/InStock.Frontend.Core/Managers/AccountManager.cs
+++ Source/InStock.Frontend/InStock.Frontend.Core/Managers/AccountManager.cs
@@ -1,6 +1,8 @@
-﻿using InStock.Common.AccountService.Abstraction.Services;
+﻿using InStock.Common.AccountService.Abstraction.Entities.ClientInfo;
+using InStock.Common.AccountService.Abstraction.Services;
 using InStock.Common.AccountService.Abstraction.TransferObjects.CreateAccount;
 using InStock.Common.AccountService.Abstraction.TransferObjects.Login;
+using InStock.Common.Models.Base;
 using InStock.Frontend.Abstraction.Managers;
 using InStock.Frontend.Abstraction.Models;
 using InStock.Frontend.Abstraction.Services.Platform;
@@ -28,32 +30,18 @@ namespace InStock.Frontend.Core.Managers
         {
             try
             {
-                var deviceId = await _settingsManager
-                .GetDeviceIdAsync()
-                .ConfigureAwait(false);
-
                 var request = new CreateAccountRequest
                 {
                     FirstName = firstName,
                     LastName = lastName,
                     Username = username,
-                    Password = password,
-                    ClientId = deviceId.Value,
-                    ClientName = _platformInfoService.ClientName,
-                    ClientDescription = _platformInfoService.ClientDescription
+                    Password = password
                 };
 
-                var result = await _accountService
-                    .CreateAccountAsync(request)
+                var result = await SendRequest(request, _accountService.CreateAccountAsync)
                     .ConfigureAwait(false);
 
-                if (result.IsSuccessfulStatusCode() && result.Data != null)
-                {
-                    await SaveTokens(result.Data)
-                        .ConfigureAwait(false);
-                }
-
-                return result.ToBooleanResult();
+                return result;
             }
             catch (Exception ex)
             {
@@ -67,21 +55,41 @@ namespace InStock.Frontend.Core.Managers
 
         public async Task<BooleanResult> LoginAsync(string? username, string? password)
         {
+            try
+            {
+                var loginRequest = new LoginRequest()
+                {
+                    Username = username,
+                    Password = password
+                };
+
+                var result = await SendRequest(loginRequest, _accountService.LoginAsync)
+                    .ConfigureAwait(false);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new BooleanResult
+                {
+                    Result = false,
+                    ErrorMessage = e.Message
+                };
+            }
+        }
+
+        public async Task<BooleanResult> SendRequest<T>(T request, Func<T, Task<Result<LoginResponse>>> method)
+            where T : IClientInfoModel
+        {
             var deviceId = await _settingsManager
                 .GetDeviceIdAsync()
                 .ConfigureAwait(false);
 
-            var loginRequest = new LoginRequest()
-            {
-                Username = username,
-                Password = password,
-                ClientId = deviceId.Value,
-                ClientName = _platformInfoService.ClientName,
-                ClientDescription = _platformInfoService.ClientDescription
-            };
+            request.ClientId = deviceId.Value;
+            request.ClientName = _platformInfoService.ClientName;
+            request.ClientDescription = _platformInfoService.ClientDescription;
 
-            var result = await _accountService
-                .LoginAsync(loginRequest)
+            var result = await method(request)
                 .ConfigureAwait(false);
 
             if (result.IsSuccessfulStatusCode() && result.Data != null)
