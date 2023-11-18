4defcbd1f7e16f98ec5873391ea21e4fc7d5da1a
Patrick Tibble <ptibble88@gmail.com>
2023-11-18 09:16:44 -0600

    add validation behavior to user input VM

---
+++ Source/InStock.Frontend/InStock.Frontend.Core/ViewModels/Base/ValidatableViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using InStock.Frontend.Abstraction.Validations;
+
+namespace InStock.Frontend.Core.ViewModels.Base
+{
+    public abstract partial class ValidatableViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private bool _isValid;
+
+        [ObservableProperty]
+        private string? _validationMessage;
+
+        [ObservableProperty]
+        private string? _text;
+
+        public IList<IValidationRule>? ValidationRules { get; set; }
+
+        protected ValidatableViewModel()
+        {
+
+        }
+
+        public virtual bool Validate()
+        {
+            if (ValidationRules == null || ValidationRules.Count == 0)
+            {
+                IsValid = true;
+                ValidationMessage = string.Empty;
+                return IsValid;
+            }
+
+            var failedRules = ValidationRules
+                .Where(rule => !rule.Check(Text))
+                .ToList();
+
+            IsValid = !failedRules.Any();
+            var messages = failedRules.Select(rule => rule.ValidationMessage);
+            ValidationMessage = string.Join(Environment.NewLine, messages);
+
+            return IsValid;
+        }
+    }
+}
