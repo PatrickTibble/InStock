fab3879017aaf3d1c9c224541e448b5746701726
Patrick Tibble <ptibble88@gmail.com>
2023-11-19 18:59:43 -0600

    update inventory service to use results

--- Source/InStock.Common/InventoryService/InStock.Common.InventoryService.Abstraction/Services/IInventoryService.cs
+++ Source/InStock.Common/InventoryService/InStock.Common.InventoryService.Abstraction/Services/IInventoryService.cs
@@ -2,6 +2,7 @@
 using InStock.Common.InventoryService.Abstraction.TransferObjects.Get;
 using InStock.Common.InventoryService.Abstraction.TransferObjects.GetAll;
 using InStock.Common.InventoryService.Abstraction.TransferObjects.Insert;
+using InStock.Common.Models.Base;
 using Refit;
 
 namespace InStock.Common.InventoryService.Abstraction.Services
@@ -9,15 +10,15 @@ namespace InStock.Common.InventoryService.Abstraction.Services
     public interface IInventoryService
     {
         [Post("/api/v1/Inventory")]
-        Task<InsertResponse> AddOrUpdateAsync(InsertRequest request);
+        Task<Result<InsertResponse>> AddOrUpdateAsync(InsertRequest request);
 
         [Delete("/api/v1/Inventory/{id}")]
-        Task<DeleteResponse> DeleteAsync(int id);
+        Task<Result<DeleteResponse>> DeleteAsync(int id);
 
         [Get("/api/v1/Inventory")]
-        Task<GetAllResponse> GetAllAsync();
+        Task<Result<GetAllResponse>> GetAllAsync();
 
         [Get("/api/v1/Inventory/{id}")]
-        Task<GetResponse> GetAsync(int id);
+        Task<Result<GetResponse>> GetAsync(int id);
     }
 }
