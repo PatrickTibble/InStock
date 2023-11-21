6b67faa5fd670a2b4c4cfbef6fae8e89d14370b8
Patrick Tibble <ptibble88@gmail.com>
2023-11-20 19:44:29 -0600

    add labels

---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Labels/Header1Label.xaml.cs
@@ -0,0 +1,9 @@
+namespace InStock.Frontend.Mobile.Views.Labels;
+
+public partial class Header1Label : Label
+{
+	public Header1Label()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Labels/ParagraphSmallLabel.xaml.cs
@@ -0,0 +1,9 @@
+namespace InStock.Frontend.Mobile.Views.Labels;
+
+public partial class ParagraphSmallLabel : Label
+{
+	public ParagraphSmallLabel()
+	{
+		InitializeComponent();
+	}
+}
