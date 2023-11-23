641c02f021cce2f0fa3a69b7c0480ab2ae9b0bfe
Patrick Tibble <ptibble88@gmail.com>
2023-11-22 22:32:39 -0600

    add converter to check null or empty strings

---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Converters/NotNullOrEmptyConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace InStock.Frontend.Mobile.Converters
+{
+    internal class NotNullOrEmptyConverter : Base.BaseValueConverter<string, bool>
+    {
+        protected override bool Convert(string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+    }
+}
