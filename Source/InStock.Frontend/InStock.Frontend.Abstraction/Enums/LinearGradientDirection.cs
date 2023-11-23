97d47b729fdff11634892d565277e5382715702f
Patrick Tibble <ptibble88@gmail.com>
2023-11-22 22:21:58 -0600

    add linear gradient direction

---
+++ Source/InStock.Frontend/InStock.Frontend.Abstraction/Enums/LinearGradientDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace InStock.Frontend.Abstraction.Enums
+{
+    public enum LinearGradientDirection
+    {
+        TopToBottom,
+        LeftToRight,
+        RightToLeft,
+        BottomToTop
+    }
+}
