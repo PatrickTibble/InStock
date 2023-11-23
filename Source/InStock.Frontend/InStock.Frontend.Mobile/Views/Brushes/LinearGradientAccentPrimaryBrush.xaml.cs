e2ae43b4d047cd5f6e82ff504d0f26f56c1b3bdd
Patrick Tibble <ptibble88@gmail.com>
2023-11-22 22:33:21 -0600

    add bindable property to be able to set direction in xaml

--- Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Brushes/LinearGradientAccentPrimaryBrush.xaml.cs
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Brushes/LinearGradientAccentPrimaryBrush.xaml.cs
@@ -1,9 +1,49 @@
+using InStock.Frontend.Abstraction.Enums;
+
 namespace InStock.Frontend.Mobile.Views.Brushes;
 
 public partial class LinearGradientAccentPrimaryBrush : LinearGradientBrush
 {
-	public LinearGradientAccentPrimaryBrush()
-	{
-		InitializeComponent();
-	}
+    public static readonly BindableProperty DirectionProperty = BindableProperty.Create(
+        nameof(Direction),
+        typeof(LinearGradientDirection),
+        typeof(LinearGradientAccentPrimaryBrush),
+        LinearGradientDirection.TopToBottom,
+        propertyChanged: (b, o, n) => ((LinearGradientAccentPrimaryBrush)b).OnDirectionChanged((LinearGradientDirection)o, (LinearGradientDirection)n));
+
+    public LinearGradientDirection Direction
+    {
+        get => (LinearGradientDirection)GetValue(DirectionProperty);
+        set => SetValue(DirectionProperty, value);
+    }
+
+    public LinearGradientAccentPrimaryBrush()
+    {
+        InitializeComponent();
+    }
+
+    private void OnDirectionChanged(LinearGradientDirection o, LinearGradientDirection n)
+    {
+        if (o == n) { return; }
+
+        switch (n)
+        {
+            case LinearGradientDirection.TopToBottom:
+                StartPoint = new Point(0.5, 0);
+                EndPoint = new Point(0.5, 1);
+                break;
+            case LinearGradientDirection.LeftToRight:
+                StartPoint = new Point(0, 0.5);
+                EndPoint = new Point(1, 0.5);
+                break;
+            case LinearGradientDirection.RightToLeft:
+                StartPoint = new Point(1, 0.5);
+                EndPoint = new Point(0, 0.5);
+                break;
+            case LinearGradientDirection.BottomToTop:
+                StartPoint = new Point(0.5, 1);
+                EndPoint = new Point(0.5, 0);
+                break;
+        }
+    }
 }
