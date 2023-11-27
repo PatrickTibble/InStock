6bf6bcdb9b5f290e264704232b8b8bb68fe8bbab
Patrick Tibble <ptibble88@gmail.com>
2023-11-26 20:33:35 -0600

    add card views for new design

---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Cards/ChartCardView.xaml.cs
@@ -0,0 +1,11 @@
+using InStock.Frontend.Mobile.Views.Frames;
+
+namespace InStock.Frontend.Mobile.Views.Cards;
+
+public partial class ChartCardView : PrimaryCardFrame
+{
+	public ChartCardView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Source/InStock.Frontend/InStock.Frontend.Mobile/Views/Cards/EntryCardView.xaml.cs
@@ -0,0 +1,11 @@
+using InStock.Frontend.Mobile.Views.Frames;
+
+namespace InStock.Frontend.Mobile.Views.Cards;
+
+public partial class EntryCardView : PrimaryCardFrame
+{
+	public EntryCardView()
+	{
+		InitializeComponent();
+	}
+}
