1e88ffe0b917805d5ee42dfece5eefd9b56de32e
Patrick Tibble <ptibble88@gmail.com>
2023-12-10 22:55:52 -0600

    add manager interfaces

---
+++ Source/InStock.Frontend/InStock.Frontend.Abstraction/Managers/ILocationsManager.cs
@@ -0,0 +1,9 @@
+﻿using InStock.Frontend.Abstraction.Models;
+
+namespace InStock.Frontend.Abstraction.Managers
+{
+    public interface ILocationsManager
+    {
+        Task<IList<Location>> GetLocationsAsync();
+    }
+}
---
+++ Source/InStock.Frontend/InStock.Frontend.Abstraction/Managers/IRevenueManager.cs
@@ -0,0 +1,9 @@
+﻿using InStock.Frontend.Abstraction.Models;
+
+namespace InStock.Frontend.Abstraction.Managers
+{
+    public interface IRevenueManager
+    {
+        Task<RevenueReport> GetRevenueReportAsync();
+    }
+}
