6fc6390fa1f0f5f4c22f87acd0cb947412107c75
Patrick Tibble <ptibble88@gmail.com>
2023-12-10 22:55:14 -0600

    add api service definitions

---
+++ Source/InStock.Common/InventoryService/InStock.Common.InventoryService.Abstraction/Services/ILocationsService.cs
@@ -0,0 +1,12 @@
+﻿using InStock.Common.InventoryService.Abstraction.TransferObjects.Locations;
+using InStock.Common.Models.Base;
+using Refit;
+
+namespace InStock.Common.InventoryService.Abstraction.Services
+{
+    public interface ILocationsService
+    {
+        [Get($"/{Constants.Locations}")]
+        Task<Result<LocationsResponse>> GetLocations([Header(Constants.Authorization)] string accessToken, CancellationToken? token = null);
+    }
+}
---
+++ Source/InStock.Common/InventoryService/InStock.Common.InventoryService.Abstraction/Services/IRevenueService.cs
@@ -0,0 +1,12 @@
+﻿using InStock.Common.InventoryService.Abstraction.TransferObjects.Revenue.Report;
+using InStock.Common.Models.Base;
+using Refit;
+
+namespace InStock.Common.InventoryService.Abstraction.Services
+{
+    public interface IRevenueService
+    {
+        [Get($"/{Constants.RevenueReport}")]
+        Task<Result<RevenueResponse>> GetRevenueReport([Header(Constants.Authorization)] string accessToken, [Query] RevenueRequest request, CancellationToken? token = null)
+    }
+}
