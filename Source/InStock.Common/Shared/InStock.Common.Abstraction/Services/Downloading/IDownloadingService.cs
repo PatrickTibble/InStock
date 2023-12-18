8ef3fabd5f6daee007689352647fe117bc99cf36
Patrick Tibble <ptibble88@gmail.com>
2023-12-17 22:36:13 -0600

    add download service

---
+++ Source/InStock.Common/Shared/InStock.Common.Abstraction/Services/Downloading/IDownloadingService.cs
@@ -0,0 +1,7 @@
+﻿namespace InStock.Common.Abstraction.Services.Downloading
+{
+    public interface IDownloadingService
+    {
+        Task<byte[]> DownloadAsync(string url, CancellationToken? token = null);
+    }
+}
