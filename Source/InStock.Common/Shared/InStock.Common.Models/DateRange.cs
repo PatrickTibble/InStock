eac6150c7393fe949bbe41bf52345035c3541233
Patrick Tibble <ptibble88@gmail.com>
2023-12-17 22:35:52 -0600

    add date range

---
+++ Source/InStock.Common/Shared/InStock.Common.Models/DateRange.cs
@@ -0,0 +1,21 @@
+﻿namespace InStock.Common.Models
+{
+    public class DateRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public double TotalDays => (End.Date - Start.Date).TotalDays;
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            if (end.Date < start.Date)
+            {
+                throw new ArgumentException("End date must some after start date");
+            }
+
+            Start = start;
+            End = end;
+        }
+    }
+}
